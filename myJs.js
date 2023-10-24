7c2877a3fa3b771ab2c450fb6b31d21015c91c02
JuneReyP <junereypalabrica@gmail.com>
2023-10-25 07:12:21 +0800

    update

---
+++ myJs.js
@@ -0,0 +1,27 @@
+
+document.getElementById("sample").innerHTML = 'sample content';
+
+//single line comment
+/* 
+this is multiline comment
+sdf
+fsd
+sdfds
+sdfdsf
+*/
+
+/*
+var
+let
+const
+*/
+var a = 2;
+var b = 5;
+var b = 4;
+
+var c = a + b;
+let d = 12;
+    d = c;
+
+    const e = 'text'
+alert(d)
