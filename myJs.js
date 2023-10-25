06a88237e535b7acdeb4023bd4967dc36219a299
JuneRey <junereypalabrica@gmail.com>
2023-10-25 09:04:36 +0800

    update

--- myJs.js
+++ myJs.js
@@ -1,5 +1,4 @@
-
-document.getElementById("sample").innerHTML = 'sample content';
+document.getElementById("sample").innerHTML = "sample content";
 
 //single line comment
 /* 
@@ -21,7 +20,44 @@ var b = 4;
 
 var c = a + b;
 let d = 12;
-    d = c;
+d = c;
 
-    const e = 'text'
-alert(d)
+const e = "text";
+// alert(d);
+/*
+Types of JavaScript Operators
+There are different types of JavaScript operators:
+
+    Arithmetic Operators
+        Operator 	Description
+        + 	        Addition
+        - 	        Subtraction
+        * 	        Multiplication
+        ** 	        Exponentiation (ES2016)
+        / 	        Division
+        % 	        Modulus (Division Remainder)
+        ++ 	        Increment
+        -- 	        Decrement
+    Assignment Operators
+        Operator 	Example 	Same As
+        = 	        x = y 	    x = y
+        += 	        x += y  	x = x + y
+        -= 	        x -= y 	    x = x - y
+        *= 	        x *= y 	    x = x * y
+        /= 	        x /= y 	    x = x / y
+        %= 	        x %= y 	    x = x % y
+        **= 	    x **= y 	x = x ** y
+    Comparison Operators
+        Operator 	Description
+        == 	        equal to        (1 == '1')
+        === 	    equal value and equal type (1 === '1')
+        != 	        not equal   (1 != '1')
+        !== 	    not equal value and not equal type   (1 !== '1')
+        > 	        greater than
+        < 	        less than
+        >= 	        greater than or equal to
+        <= 	        less than or equal to
+        ? 	        ternary operator
+    Logical Operators
+    Ternary Operators
+*/
