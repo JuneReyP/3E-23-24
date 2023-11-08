b92c3e5ccbd8bb9716e62f28bb38f5f8e5c4681f
JuneRey <junereypalabrica@gmail.com>
2023-11-08 09:11:58 +0800

    discussion for conditional statement

--- myJs.js
+++ myJs.js
@@ -68,22 +68,138 @@ There are different types of JavaScript operators:
         (condition) ? ifTrue : ifFalse;
 */
 // function without parameter
-function myFunc(){
-    let getID = document.getElementById('sample');
-    getID.style.backgroundColor = 'red';
-    getID.style.border = "1px solid blue";
-    getID.style.color = "orange";
+function myFunc() {
+  let getID = document.getElementById("sample");
+  getID.style.backgroundColor = "red";
+  getID.style.border = "1px solid blue";
+  getID.style.color = "orange";
 }
 
 // myFunc();
 // with parameter
-function sumOf(num1, num2){
-    let ans = parseInt(num1) + parseInt(num2);
-    document.getElementById('answer').innerHTML = ans;
+function sumOf(num1, num2) {
+  let ans = parseInt(num1) + parseInt(num2);
+  document.getElementById("answer").innerHTML = ans;
 }
-function simple(){
-    let num1 = document.getElementById('num1').value;
-    let num2 = document.getElementById('num2').value;
+function simple() {
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
 
-    sumOf(parseInt(num1), parseInt(num2));
+  sumOf(parseInt(num1), parseInt(num2));
+}
+
+/**
+ * Conditional statements
+if statement
+if else statement
+if else-if else
+switch case
+for loop
+while loop
+ */
+
+//array
+// let myArr = ["Apple", "orange", "banana"];
+// document.write(myArr[0] + "<br>");
+// document.write(myArr[1] + "<br>");
+// document.write(myArr[2] + "<br>");
+
+/**
+ if(condition){
+    //code executed if the condition is true
+ }
+ */
+
+//  let grade = 89;
+//  if(grade > 90){
+//     alert("Your mark is A+")
+//  }
+
+/**
+if (condition) {
+  //  block of code to be executed if the condition is true
+} else {
+  //  block of code to be executed if the condition is false
+}
+ */
+
+// let grade = 89;
+// if(grade > 90){
+//    alert("Your mark is A+");
+// //    if(){
+
+// //    }
+// }else{
+//     alert("Your mark is below 90");
+// }
+
+/**
+if (condition1) {
+  //  block of code to be executed if condition1 is true
+} else if (condition2) {
+  //  block of code to be executed if the condition1 is false and condition2 is true
+} else {
+  //  block of code to be executed if the condition1 is false and condition2 is false
+}
+ */
+
+// let grade = 99;
+// if (grade >= 90) {
+//   if (grade > 100) {
+//     alert("You scored above 100 marks, that's impossible!");
+//   } else {
+//     alert("Your mark is A+");
+//   }
+// } else if (grade < 90 && grade >= 80) {
+//   alert("Your mark is B");
+// } else if (grade >= 75) {
+//   alert("Your mark is C");
+// } else {
+//   alert("You Failed!");
+// }
+
+/**
+switch(x) {
+  case x:
+    // code block
+    break;
+  case y:
+    // code block
+    break;
+  default:
+    // code block
+}
+ */
+// let num1 = 3;
+// let num2 = 4;
+// let expression = "+";
+// switch(expression) {
+//     case '+':
+//       alert(num1 + num2);
+//     //   break;
+//     case '*':
+//         alert(num1 * num2);
+//     //   break;
+//     case '-':
+//         alert(num1 - num2);
+//         break;
+//     case '/':
+//         alert(num1 / num2);
+//         break;
+//     default:
+//         alert('Invalid operator');
+//         break;
+// }
+
+/**
+for (initializer; condition; iterator) {
+  // code block to be executed if condition is true
+}
+ */
+
+let myArr = ["Apple", "orange", "banana", "grapes", "Guava"];
+// alert(myArr[3])
+
+for(let a = 0; a > 5; a++){
+    document.write(myArr[a] + "<br>");
 }
